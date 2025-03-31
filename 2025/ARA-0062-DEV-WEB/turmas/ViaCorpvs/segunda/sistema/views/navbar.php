68efa4d8b80a8d717b9af1405bba23a8085dd580
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2025-03-31 20:09:04 -0300

    ViaCorpvs-segunda: Aula 05 - parte 01

---
+++ 2025/ARA-0062-DEV-WEB/turmas/ViaCorpvs/segunda/sistema/views/navbar.php
@@ -0,0 +1,32 @@
+<hr>
+<nav class="row align-items-center">
+    <div class="col">
+        <span class="fw-bold fs-3">
+            <i class="material-icons fs-1">sports_bar</i>
+            FIC BEBO
+        </span>
+    </div>
+    <div class="col text-end">
+        <a class="btn btn-outline-dark" href="dashboard.html">
+            <i class="material-icons">home</i>
+            Dashboard
+        </a>
+        <a class="btn btn-outline-dark" href="clientes.html">
+            <i class="material-icons">group</i>
+            Clientes
+        </a>
+        <a class="btn btn-outline-dark" href="produtos.html">
+            <i class="material-icons">liquor</i>
+            Produtos
+        </a>
+        <a class="btn btn-outline-dark" href="pedidos.html">
+            <i class="material-icons">shopping_cart</i>
+            Pedidos
+        </a>
+        <a class="btn btn-outline-danger" href="index.html">
+            <i class="material-icons">logout</i>
+            Sair
+        </a>
+    </div>
+</nav>
+<hr>
